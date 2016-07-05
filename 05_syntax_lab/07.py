8eee97a44699e65017f428ca80f29db751b5ad28
EyalAbel <abel.eyal@gmail.com>
2016-07-05 18:26:51 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,8 +1,58 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
+#!/usr/bin/env python
 
-After each guess print a hint "too large" or "too small" to the user.
+__author__ = 'Eabel'
 
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+import sys
+from random import randint
 
+def IsDivisibleBy(n,d):
+   return (n%d == 0)
+
+def compare(x,y):
+    if x == y:
+        return 0
+    elif x < y:
+        return 1
+    else:
+        return -1
+
+def compareWithPossibleMistake(x,y):
+    num = randint(1,100);
+    comp = compare(x,y);
+    if not IsDivisibleBy(num,7):
+        return comp
+    else:    
+        if comp == -1:
+            return 1;
+        elif comp == 1:
+            return -1
+        else:
+            return comp
+
+
+def Exercise7():
+    num = randint(1,100);
+    ansMap = {1: "Too big, try again.", -1:"Too small, try again.", 0:"You got a match."}
+    
+    print "Please guess a number between 1 to 100:"
+
+    while True:
+        try:
+            compRes = compareWithPossibleMistake(num,int(raw_input()))
+            print ansMap[compRes]
+            if not compRes:
+                break
+        except ValueError:
+            print "Please enter only numbers"
+            sys.exit()  
+
+########################################################################################################################
+#
+#
+def main():
+    Exercise7()
+
+           
+
+if __name__ == '__main__':
+    main()
