b5098bdf9cd46721ba97458a214ec7ed0f4ac824
EyalAbel <abel.eyal@gmail.com>
2016-07-05 18:26:50 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,27 @@
-"""
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
-"""
+#!/usr/bin/env python
 
+__author__ = 'Eabel'
+
+import sys
+from random import randint
+
+def IsDivisibleBy(n,d):
+   return (n%d == 0)
+
+def Exercise5():
+    while True:
+        rand_num = randint(1,1000000);       
+        if IsDivisibleBy(rand_num,15) and IsDivisibleBy(rand_num,13) and IsDivisibleBy(rand_num,7):
+            print rand_num,"Divisible by 7, 13 and 15"
+            break;
+
+########################################################################################################################
+#
+#
+def main():
+    Exercise5()
+
+           
+
+if __name__ == '__main__':
+    main()
