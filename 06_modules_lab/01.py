cecf0d59c01033df1bfcc2c6eecbe7094830c537
EyalAbel <abel.eyal@gmail.com>
2016-07-06 10:07:29 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,26 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+#!/usr/bin/env python
+
+__author__ = 'Eabel'
+
+import sys
+
+
+def Usage():
+    print ('Invalud arguments.\nUsage: 01.py N')
+########################################################################################################################
+#
+#
+def main():
+    if len(sys.argv) != 2:
+        Usage()
+        sys.exit()
+    try:
+        for i in range(int(sys.argv[1])):
+            print 'Hello Python'
+    except ValueError:
+        Usage()
+        sys.exit()  
+           
+
+if __name__ == '__main__':
+    main()
