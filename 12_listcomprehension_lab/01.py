065e7259e87e7ff75a813d742d959dbaedd34ca2
EyalAbel <abel.eyal@gmail.com>
2016-07-09 16:01:13 +0300

    Solution for: sol-python-verint.list-comprehension-lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+
 """
-Use range() and list comprehension to get
-the list of all lowercase english letters
-Hint: look for chr() and ord()
+This program prints the english alphabet (not capital letters) 
 """
+__author__ = 'Eabel'
+
+import sys
+import os
+from itertools import permutations
 
 
 
+########################################################################################################################
+#
+#
+def main():
+    alphabet = [chr(x) for x in range(ord('a'),ord('z')+1)]
+    print alphabet;
+      
+if __name__ == '__main__':
+    main()
