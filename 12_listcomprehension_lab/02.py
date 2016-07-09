6fdcb185754dfcd72705e22388a03cb0122f2b7d
EyalAbel <abel.eyal@gmail.com>
2016-07-09 16:01:14 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,21 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
+#!/usr/bin/env python
 
+"""
+This program prints the english alphabet non capital letters permutations of length 3 with repeats 
+"""
+__author__ = 'Eabel'
+
+import sys
+import os
+import itertools
+
+
+########################################################################################################################
+#
+#
+def main():
+    alphabet = [chr(x) for x in range(ord('a'),ord('z')+1)]
+    print '\n'.join([''.join(p) for p in itertools.product(alphabet, repeat=3)])
+      
+if __name__ == '__main__':
+    main()
