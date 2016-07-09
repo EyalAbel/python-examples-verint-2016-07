d4087577b2d0e9e07e67175f29395f9197f3c002
EyalAbel <abel.eyal@gmail.com>
2016-07-09 13:42:28 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,43 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+#!/usr/bin/env python
 
+"""
+This program concatinate file_1 into file_2
+
+"""
+__author__ = 'Eabel'
+
+import sys
+import os
+import argparse
+
+def is_valid_file_path(file_path):
+    if os.path.isfile(file_path):
+        return True
+    else :      
+        print "Error - invalid file path",file_path
+        return False
+
+########################################################################################################################
+#
+#
+def main():
+    path  = "."
+    parser = argparse.ArgumentParser(description='This program concatinate file_1 into file_2')
+    
+    parser.add_argument("file_1", help="path to file")
+    parser.add_argument("file_2", help="path to file")    
+    args = parser.parse_args()
+    if not is_valid_file_path(args.file_1) or not is_valid_file_path(args.file_2):
+        sys.exit()
+
+    with open(args.file_1, "r") as src:
+        with open(args.file_2, "a") as dst:
+            dst.write(src.read())
+        
+        
+
+    
+           
+
+if __name__ == '__main__':
+    main()
