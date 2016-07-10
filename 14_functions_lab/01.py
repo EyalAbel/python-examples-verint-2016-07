dc3c0d876054a7462dacafbd3a046811f783c238
EyalAbel <abel.eyal@gmail.com>
2016-07-10 10:22:20 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,36 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+#!/usr/bin/env python
+
+__author__ = 'Eabel'
+
+import sys
+import os
+import numbers
+
+
+
+def mysum(*nums):
+    """
+    Args:
+        nums (int list): numbers to accumulate (ignore non integers arguments)
+    Returns:
+        int: Return an sum of the arguments
+    """
+    sum = 0
+    for num in nums:
+        if isinstance(num, numbers.Number):
+            sum += num
+    return sum
+
+def mymul(*nums):
+    """
+    Args:
+        nums (int list): numbers to multiply (ignore non integers arguments)
+    Returns:
+        int: Return an multiplaction of the arguments
+    """
+    mul = 1
+    for num in nums:
+        if isinstance(num, numbers.Number):
+            mul *= int(num)
+    return mul
 
