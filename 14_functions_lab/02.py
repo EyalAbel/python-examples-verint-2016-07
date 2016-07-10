b896da31a17641b4001c82046b71871785b8a97b
EyalAbel <abel.eyal@gmail.com>
2016-07-10 10:22:21 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,23 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+#!/usr/bin/env python
+
+__author__ = 'Eabel'
+
+import sys
+import os
+import numbers
+
+
+def foo(str_arg, num_arg):
+    """
+    Args:
+        str_arg (string): string arg
+        num_arg (string): number arg
+    Returns:
+        int: prints the args
+    """
+    if not isinstance(num_arg, numbers.Number) or not isinstance(str_arg, basestring):
+        raise ValueError("Invalid arguments type")
+    print str_arg,num_arg
+
+
 
