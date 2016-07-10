1b547b2626d642756496097ffd2260f53488bbe7
EyalAbel <abel.eyal@gmail.com>
2016-07-10 10:22:22 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,36 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+#!/usr/bin/env python
 
+
+__author__ = 'Eabel'
+
+import sys
+import os
+
+
+
+def sum_second_digits(*nums):
+    """
+    Args:
+        nums (int list): list of numbers
+    Returns:
+        int: sum the second digit of each number
+    """
+    try:
+        return sum( [ (number / 10) % 10 for number in nums ] )
+    except:
+        e = sys.exc_info()[0]
+        raise ValueError(e)
+
+
+########################################################################################################################
+#
+#
+def main():
+    sum_second_digits(445,325,111)
+    sum_second_digits(445,'443',111)
+ 
+
+
+      
+if __name__ == '__main__':
+    main()
