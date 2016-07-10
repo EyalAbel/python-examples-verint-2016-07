31882fc11e1a9b4b092600f4c6a4f30784f2d0b6
EyalAbel <abel.eyal@gmail.com>
2016-07-10 16:38:58 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -1,12 +1,35 @@
-"""
-Write a program that reads data
-from property files.
-Each line in the file can either be:
-    An empty line
-    A comment line (Start with #)
-    A property line (of the form key = value)
+#!/usr/bin/env python
 
-Write a program that takes a property file name and key
-as command line arguments and prints the requested value
-"""
+__author__ = 'Eabel'
 
+import sys
+import os
+import argparse
+import re
+
+
+########################################################################################################################
+#
+#
+def main():
+    parser = argparse.ArgumentParser(description='This program receive config file path and varible name and prints the varible value')
+    
+    parser.add_argument("config_path", help="path to config file")
+    parser.add_argument("varible_name", help="varible name")
+    args = parser.parse_args()
+
+    try:
+        regex_str = '\s*' + args.varible_name + '\s*=\s*(.*)'
+        with open(args.config_path,'r') as f:
+            for line in f:   
+                res = re.search(regex_str, line)
+                if res is not None:
+                    print  args.varible_name,'value is',res.group(1)
+                    return
+        print 'No match for',args.varible_name
+    except:
+        e = sys.exc_info()[0]
+        print "Error loading file -",e
+                         
+if __name__ == '__main__':
+    main()
